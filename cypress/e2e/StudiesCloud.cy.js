b51a3a1f4c99cbd10d4c9598150c93946dae3056
Marcelo-OM30 <marcelo.salmeron@om30.com.br>
2025-04-11 09:04:14 -0300

    alteracao

--- cypress/e2e/StudiesCloud.cy.js
+++ cypress/e2e/StudiesCloud.cy.js
@@ -2,6 +2,6 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://marcelosal.github.io/tennis-school/')
     cy.get('.title h1 span')
-      .should('have.text', 'TENIS')
+      .should('have.text', 'TENNIS')
   })
 })
